feb0a7f6f2b35ea7610d871d6bd0a1f687af6472
HudahanAltn <dev.hudahanaltun@gmail.com>
2023-01-13 21:19:41 +0300

    creating new table view and connected view controller. Added some methods to test

--- ToDo/ViewController.swift
+++ ToDo/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var toDoTableView: UITableView!
+    
+    let doItemList:[String] = ["take the dog for a walk","go shopping","buy new clothes"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        toDoTableView.delegate = self
+        toDoTableView.dataSource = self
     }
 
 
 }
 
+
+extension ViewController:UITableViewDataSource,UITableViewDelegate{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return doItemList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell",for:indexPath)
+        
+        cell.textLabel?.text = doItemList[indexPath.row]
+        
+        return cell
+    }
+}
